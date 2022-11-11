7db228e2038f994130cb32f260da9a99d6ce3597
fmuklis@gmail.com <fmuklis@gmail.com>
2022-11-12 00:18:22 +0700

    Initializations

---
+++ Gatherly.Application/Invitations/Commands/AcceptInvitation/AcceptInvitationCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using Gatherly.Application.Abstractions;
+using Gatherly.Domain.Enums;
+using Gatherly.Domain.Repositories;
+using MediatR;
+
+namespace Gatherly.Application.Invitations.Commands.AcceptInvitation;
+
+public class AcceptInvitationCommandHandler : IRequestHandler<AcceptInvitationCommand>
+{
+    private readonly IGatheringRepository _gatheringRepository;
+    private readonly IAttendeeRepository _attendeeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IEmailService _emailService;
+
+    public AcceptInvitationCommandHandler(IGatheringRepository gatheringRepository,
+        IAttendeeRepository attendeeRepository,
+        IUnitOfWork unitOfWork,
+        IEmailService emailService)
+    {
+        _gatheringRepository = gatheringRepository;
+        _attendeeRepository = attendeeRepository;
+        _unitOfWork = unitOfWork;
+        _emailService = emailService;
+    }
+    public async Task<Unit> Handle(AcceptInvitationCommand request, CancellationToken cancellationToken)
+    {
+        var gathering = await _gatheringRepository.GetByIdWithCreatorAsync(request.gatheringId, cancellationToken);
+
+        if (gathering is null)
+        {
+            return Unit.Value;
+        }
+
+        var invitation = gathering.Invitations.FirstOrDefault(i => i.Id == request.invitationId);
+
+        if (invitation is null || invitation.Status != InvitationStatus.Pending)
+        {
+            return Unit.Value;
+        }
+
+        var attendeeResult = gathering.AcceptInvitation(invitation);
+
+        if (attendeeResult.IsSuccess)
+        {
+            _attendeeRepository.Add(attendeeResult.Value!);
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ Gatherly.Domain/Shared/Error.cs
@@ -0,0 +1,78 @@
+﻿using Gatherly.Domain.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatherly.Domain.Shared;
+
+public class Error : IEquatable<Error>
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "The Specified result value is null.");
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+    public string Message { get; }
+
+    public static implicit operator string(Error error) => error.Code;
+
+    public static bool operator ==(Error? a, Error? b)
+    {
+        if (a is null && b is null)
+        {
+            return true;
+        }
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
+    public static bool operator !=(Error? a, Error? b)
+    {
+        return !(a == b);
+    }
+    public bool Equals(Error? other)
+    {
+        if (other is null) { return false; }
+
+        if (other.GetType() != GetType()) { return false; }
+
+        if (other is not Error error) { return false; }
+
+        return error.Code == Code;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Error error)
+        {
+            return false; 
+        }
+
+        return error.Code == Code;
+    }
+
+    public override int GetHashCode()
+    {
+        return Code.GetHashCode() * 24;
+    }
+}
